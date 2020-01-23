e4879f8a50f6f0c40c3906776611ba0e971e7dbf
Mathias Baumann <git@supradigital.org>
2020-01-23 13:58:33 +0100

    fix test for homestead

--- test/libsolidity/syntaxTests/functionCalls/lowlevel_call_options.sol
+++ test/libsolidity/syntaxTests/functionCalls/lowlevel_call_options.sol
@@ -1,8 +1,7 @@
 contract C {
     function foo() internal {
-        (bool success, bytes memory data) = address(10).call{value: 7, gas: 3}("");
+        (bool success, ) = address(10).call{value: 7, gas: 3}("");
         success;
-        data;
     }
 }
 // ----
