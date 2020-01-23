ae9be1088e69239926e6b90bb09442db7761bf08
Mathias Baumann <git@supradigital.org>
2020-01-23 13:02:50 +0100

    Fix tests to work with new type checks

--- test/libsolidity/syntaxTests/functionCalls/new_with_calloptions.sol
+++ test/libsolidity/syntaxTests/functionCalls/new_with_calloptions.sol
@@ -1,10 +1,10 @@
 contract D { constructor() public payable {} }
 contract C {
     function foo() pure internal {
-		new D{salt:5, value:3};
-		new D{salt:5};
+		new D{salt:"abc", value:3};
+		new D{salt:"abc"};
 		new D{value:5+5};
-		new D{salt:2130+5};
+		new D{salt:"aabbcc"};
     }
 }
 // ----
